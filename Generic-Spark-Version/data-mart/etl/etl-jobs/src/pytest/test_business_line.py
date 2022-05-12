9dcec23e51f9d298ecf9e44c240a9d4b39c6de9c
gbhogle1789 <gbhogle1789@gmail.com>
2022-05-12 18:21:50 +0530

    sample pytest on business_line

---
+++ Generic-Spark-Version/data-mart/etl/etl-jobs/src/pytest/test_business_line.py
@@ -0,0 +1,61 @@
+import pytest
+import pyspark
+from pyspark.sql import SparkSession
+import importlib
+from typing import Dict, List, Any
+import common.utils
+import os
+import logging
+import sys
+
+
+class test_sample:
+
+    @pytest.mark.usefixtures("spark_session")
+    def test_sample_transform(self):
+        spark = SparkSession \
+            .builder \
+            .appName("etltest") \
+            .getOrCreate()
+        import configparser
+        config = configparser.ConfigParser()
+        config.read(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'D:\\Spark_automation_framework\\spark-automation-framework\\Generic-Spark-Version\\data-mart\\etl\\etl-jobs\\scripts\\environment.ini'))
+
+        catalog: Dict[str, Any] = common.utils.get_catalog()
+
+        env: Dict[str, Any] = {
+            "jdbc":
+                {"url": config['db']['url'],
+                 "database": config['db']['database'],
+                 "user": config['db']['user'],
+                 "password": config['db']['password'],
+                 "schema": 'hdm'
+                 },
+            "folders": {},
+            "catalog": catalog
+        }
+        env["file_prefix"] = "D:\\Spark_automation_framework\\spark-automation-framework\\Generic-Spark-Version\\data-mart\\etl\\etl-jobs\\src\\jobs\\%s\\%s\\tests\\data" % ('fees','business_line')
+        logging.basicConfig()
+        logger = logging.getLogger('fees.business_line')
+        # level = logging.getLevelName(loglevel)
+        # logger.setLevel(loglevel)
+        print("hello")
+
+        print(catalog)
+
+        from common.etl_job import ETLJob  # must be imported after spark has been set up
+        job_module = importlib.import_module("jobs.%s.job" % 'fees.business_line')
+        job_class = getattr(job_module, "Job")
+        job: ETLJob = job_class(spark, env, logger, [])
+        # inputs:  pyspark.sql.DataFrame = job.extract(catalog)
+        inputs: Dict[str, pyspark.sql.DataFrame] = job.extract(env["catalog"])
+        # join the dataframe
+        df_joined: pyspark.sql.DataFrame = job.join(inputs)
+        # transform to target structure
+        df_target: pyspark.sql.DataFrame = job.transform(df_joined)
+        out_df = spark.read.parquet("D:\\Spark_automation_framework\\spark-automation-framework\\Generic-Spark-Version\\data-mart\\etl\\etl-jobs\\src\\jobs\\fees\\business_line\\tests\\data\\datamart\\business_line")
+        assert (df_target.count() == out_df.count()),"Count test not passed"
+        print("Count test passed")
+
+
+    test_sample_transform('')
